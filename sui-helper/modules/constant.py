82228b6ef78ffb4852844ab44309672ac889129c
Irors <81914651+Irors@users.noreply.github.com>
2023-09-29 17:17:52 +0500

    Update constant.py

--- sui-helper/modules/constant.py
+++ sui-helper/modules/constant.py
@@ -3,4 +3,4 @@ usdc = "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin
 suis = "0xefe8b36d5b2e43728cc323298626b83177803521d195cfb11e15b910e892fddf::reserve::MarketCoin<0x2::sui::SUI>"
 
 gas_budget = "10000000"
-timeS = [3, 5]
+timeS = [3, 5]            # Random from array
