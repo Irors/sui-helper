bd6c7064fbab63c11e11768dbf67ac4ab33c24db
Irors <81914651+Irors@users.noreply.github.com>
2023-09-29 17:19:50 +0500

    Update settings.py

--- sui-helper/settings.py
+++ sui-helper/settings.py
@@ -3,4 +3,4 @@ from modules.constant import sui
 rpc = "https://sui-mainnet-rpc.allthatnode.com"     # Input rpc sui
 token = sui                                         # Select active token
 amount = 0.01                                       # Input amount token
-custom_route = ['merge_coin', 'split_coin']         # Input module route
+custom_route = ['merge_coin', 'split_coin']         # Input module route. Available: [merge_coin, split_coin, create_gas_object, get_balance, Withdraw_scallo_protocol]
